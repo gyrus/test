7505a3154e55c127c324973d12059363d2adaed1
Steve Taylor <steve@sltaylor.co.uk>
2015-02-10 22:01:20 +0000

    0.1.2

--- test.php
+++ test.php
@@ -4,12 +4,12 @@
  * Plugin Name:			Gyrus Test
  * Plugin URI:			https://github.com/gyrus/test
  * Description:			A plugin for testing GitHub.
- * Version:				0.1.1
+ * Version:				0.1.2
  * Author:				Gyrus
  * GitHub Plugin URI:	https://github.com/gyrus/test
  */
 
 add_action( 'twentyfifteen_credits', 'gt_twentyfifteen_credits' );
 function gt_twentyfifteen_credits() {
-	echo '<p>Test plugin version 0.1.1</p>';
+	echo '<p>Test plugin version 0.1.2</p>';
 }
